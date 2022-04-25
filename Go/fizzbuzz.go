dcc3462eb9f2197d1443acdd261bd5fcf42afec8
Rayrsn <25773909+Rayrsn@users.noreply.github.com>
2022-04-25 15:06:08 +0430

    Added Go

---
+++ Go/fizzbuzz.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var numcount string
+
+	fmt.Print("How many numbers to display: ")
+	fmt.Scanf("%s", &numcount)
+	intnumcount, err := strconv.Atoi(numcount)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	for i := 1; i <= intnumcount; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+
+}
